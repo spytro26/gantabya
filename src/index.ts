ec8cbd7a004448ea914872d4c30038ac960618cb
spytro26 <2654anku@gmail.com>
2025-12-16 01:17:27 +0530

    vercel2

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [
   "http://localhost:5173",
   "http://localhost:3000",
   "https://gogantabya.netlify.app",
-  "https://gantabya-front-git-main-hars-projects-5e449a73.vercel.app/",
+  "https://gantabya-front-git-main-hars-projects-5e449a73.vercel.app",
   "https://gogantabya.com",
 ];
 
